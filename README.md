9ab89b456ab7704dbbfc9f7505291f1e6d2f5818
Rassel <Rassel>
2020-06-20 10:49:45 +0300

    Fixed work with requirements file

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 
 ### Description
 ***
-Utility for install python packages from requirements.txt on __Windows__.
-Standard 'pip' stopped installation if there were some errors in one of packages.
-This util installs packages and print a list of installed and failed packages.
+Utility allows to install python packages from requirements.txt on __Windows__.
+Standard 'pip' stops installation, if there were some errors in one of packages.
+This tool installs and print a list of installed and failed packages.
 
 **WARNING!** Use this under your responsibility. Only for __Windows__.
 
 ### Usage
 ***
-Clone repository and run - then this util can remove.
+Commands by queue:
 ```
 git clone https://github.com/RasselJohn/win-package-installer.git
 cd win-package-installer
--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import subprocess
 import sys
 from traceback import format_exc
 
-parser = argparse.ArgumentParser(description='Install packages from file of requirements.')
+parser = argparse.ArgumentParser(
+    description='Install packages from file of requirements.'
+)
 parser.add_argument('packages_file', metavar='File', type=str, help='Path to file')
 
 if len(sys.argv) != 2:
-    print('Incorrect command. For help use `python.exe <current_script>.py -h')
+    print('Incorrect command. For help use `python.exe <current_script>.py -h`')
     exit(1)
 
 args = parser.parse_args()
@@ -21,12 +23,18 @@ except FileNotFoundError:
 
 success_installed = []
 fail_installed = {}
-for package_name in req_file:
-    package_name = package_name.rstrip('\n')
+
+for package in req_file:
+    package_name = package.strip()
+    if not package_name or package_name.startswith('#'):
+        continue
+
     try:
         print(f'Installing {package_name}...')
         result = subprocess.run(
-            [sys.executable, '-m', 'pip', 'install', package_name], stdout=subprocess.DEVNULL, stderr=subprocess.PIPE
+            [sys.executable, '-m', 'pip', 'install', package_name],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.PIPE
         )
 
         if result.returncode == 0:
@@ -38,18 +46,18 @@ for package_name in req_file:
         print(f'Unknown error:{format_exc()}')
         exit(1)
 
-print(f'\nInstalling packages is completed. Results:')
+print(f'\nInstalling packages is completed.')
 
 if success_installed:
-    print(f'Next packages were installed successfully:\n', '; '.join(success_installed))
+    print(f'\nNext packages were installed successfully:\n', '; '.join(success_installed))
 
 if fail_installed:
-    print(f'\nNext packages were not installed from errors:', '; '.join(fail_installed.keys()))
+    print(f'\nNext packages were not installed from errors:\n', '; '.join(fail_installed.keys()))
 
 req_file.close()
 
 if input('Show errors(y/n)?') == 'y':
-    print('')
+    print()
     for k, v in fail_installed.items():
         print(f'Package: {k}')
         print(f'Error: {v}')
